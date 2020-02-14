6a4741bae737f636e591d576e9f88d53a4865829
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 16:26:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |item|
-    array.each do |x|
+    array.find do |x|
       x == item
     end
   end
